eee764f8f1b42c87203c4bcce554b41a2d06d362
vishal2082 <vishalbhardwaj070@gmail.com>
2018-03-27 18:42:38 +0530

    committing to local

---
+++ src/test/java/com/codebind/HomeTest.java
@@ -0,0 +1,13 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeTest {
+
+	public static void main(String[] args) {
+		System.out.println("Home Test executed...");
+	}
+
+}
